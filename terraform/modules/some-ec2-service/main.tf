2dfde346fa7cc6a38cfa522ec6e1b0f50e295e5a
Andrew Walker <walkerab.eml@gmail.com>
2021-11-04 17:33:47 -0700

    fmt

--- terraform/modules/some-ec2-service/main.tf
+++ terraform/modules/some-ec2-service/main.tf
@@ -15,8 +15,8 @@ data "aws_ami" "amazon_linux_2_ami" {
 data "aws_default_tags" "this" {}
 
 resource "aws_instance" "this" {
-  ami             = data.aws_ami.amazon_linux_2_ami.id
-  instance_type   = "t3.micro"
+  ami           = data.aws_ami.amazon_linux_2_ami.id
+  instance_type = "t3.micro"
 
   tags = {
     Name        = "${data.aws_default_tags.this.tags.Environment}-some-ec2-service",
